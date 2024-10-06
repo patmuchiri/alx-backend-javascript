7b381b8416733f91c239c2b60529208781de4e31
patmuchiri <pmuchirig@gmail.com>
2024-10-06 12:59:46 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    const task = false;
+    const task2 = true;
 
     // Correct the variable name to match the parameter
     if (trueOrFalse) {
-        let task = true;  // This shadows the outer task variable
-        let task2 = false; // This shadows the outer task2 variable
+        const task = true;  // This shadows the outer task variable
+        const task2 = false; // This shadows the outer task2 variable
     }
 
     return [task, task2]; // Returns the outer task and task2
