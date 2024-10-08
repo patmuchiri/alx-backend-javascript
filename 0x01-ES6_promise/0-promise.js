b113138e09ca777644592aad47f7569fbdf9a32f
patmuchiri <pmuchirig@gmail.com>
2024-10-08 15:18:31 +0300

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation using setTimeout
-    setTimeout(() => {
-      // Simulate success by resolving the promise
-      resolve("Data fetched successfully!");
-    }, 2000); // 2-second delay
-  });
+export default function getResponseFromAPI() {
+	return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
